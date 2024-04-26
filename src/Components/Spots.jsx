e2073b9586db138e83441ee9ea89722f2654de08
sajib689 <jibonraja11@gmail.com>
2024-04-26 16:38:34 +0600

    add spot card

--- src/Components/Spots.jsx
+++ src/Components/Spots.jsx
@@ -1,11 +1,23 @@
-
+import { useEffect, useState } from "react";
+import SpotsCard from "./SpotsCard";
 
 const Spots = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [touristSpots, setTouristSpots] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/spots")
+      .then((res) => res.json())
+      .then((data) => setTouristSpots(data));
+  }, []);
+  return (
+    <div>
+      <h1>Tourists Spots</h1>
+      <div>
+        {touristSpots.map((spot) => (
+          <SpotsCard key={spot._id} spot={spot}></SpotsCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Spots;
---
+++ src/Components/SpotsCard.jsx
@@ -0,0 +1,11 @@
+
+
+const SpotsCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SpotsCard;
