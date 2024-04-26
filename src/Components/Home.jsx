9dbf7efbd5f82e537bd0669f1b263b0070753abb
sajib689 <jibonraja11@gmail.com>
2024-04-26 12:25:12 +0600

    add a new section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import Team from "./Team";
 
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Team/>
         </div>
     );
 };
