4a37ee932e191398114080d9112dda610379a670
sajib689 <jibonraja11@gmail.com>
2024-04-26 17:14:19 +0600

    some change in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,16 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/router.jsx";
+import AuthProvider from "./Provider/AuthProvider.jsx";
+import { Toaster } from "react-hot-toast";
 
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Router/router.jsx'
-import AuthProvider from './Provider/AuthProvider.jsx'
-import { Toaster } from 'react-hot-toast';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='max-w-6xl mx-auto font-nunita'>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-6xl mx-auto font-nunita">
     <AuthProvider>
-    <RouterProvider router={router}>
-    <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
-    </RouterProvider>
+      <RouterProvider router={router}>
+        <Toaster />
+      </RouterProvider>
     </AuthProvider>
-  </div>,
-)
+  </div>
+);
