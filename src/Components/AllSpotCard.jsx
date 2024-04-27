c6dcd70a10ed3e67e569487de634f6451d9cf68c
sajib689 <jibonraja11@gmail.com>
2024-04-27 10:49:04 +0600

    add all spot page card

---
+++ src/Components/AllSpotCard.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+
+const AllSpotCard = ({ spot }) => {
+  const {
+    _id,
+    image,
+    tourist_spot_name,
+    short_description,
+    average_cost,
+    total_visitors_per_year,
+    travel_time,
+    seasonality
+  } = spot;
+  return (
+    <div className="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+      <img
+        src={image}
+        alt=""
+        className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+      />
+      <div className="flex flex-col justify-between p-6 space-y-8">
+        <div className="space-y-2">
+          <h2 className="text-3xl font-semibold tracking-wide">
+            {tourist_spot_name}
+          </h2>
+          <p className="dark:text-gray-800">
+            {short_description.slice(0, 60)}...
+          </p>
+          <div>
+            <div>
+                <p><span className="font-bold">Average Cost:</span> {average_cost} per person</p>
+                <p><span className="font-bold">Travel Time:</span> {travel_time}</p>
+            </div>
+            <div>
+                <p><span className="font-bold">Total Visitors Per Year:</span> {total_visitors_per_year}</p>
+                <p><span className="font-bold">Seasonality:</span> {seasonality}</p>
+            </div>
+          </div>
+        </div>
+        <Link
+          to={`/details/${_id}`}
+          className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-[#fc5a34] text-gray-50"
+        >
+          View Details
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default AllSpotCard;
--- src/Components/AllSpots.jsx
+++ src/Components/AllSpots.jsx
@@ -1,13 +1,23 @@
 import { useLoaderData } from "react-router-dom";
+import Loader from "../Utilitis/Loader";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import AllSpotCard from "./AllSpotCard";
 
 
 const AllSpots = () => {
+    const{loading} = useContext(AuthContext)
     const spots = useLoaderData()
-    console.log(spots)
+    if(loading) return <Loader/>
     return (
-        <div>
-            
-        </div>
+        <div className="mt-24 mb-24">
+      <h1 className="text-4xl text-start mb-20 font-bold">All Tourists Spots</h1>
+      <div className="grid grid-cols md:grid-cols-3 gap-6">
+        {spots.map((spot) => (
+          <AllSpotCard key={spot._id} spot={spot}></AllSpotCard>
+        ))}
+      </div>
+    </div>
     );
 };
 
