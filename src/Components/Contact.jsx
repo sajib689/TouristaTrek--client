162580eead3e9a82ca1792fc2cf725fbcd19a198
sajib689 <jibonraja11@gmail.com>
2024-04-28 11:22:10 +0600

    desgin about and contact pages

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,13 @@
+import Lottie from "lottie-react";
+import contact from '../../public/contact.json'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
 const Contact = () => {
   return (
     <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
       <div className="flex flex-col justify-between">
-        <div className="space-y-2">
+        <div data-aos="fade-down-right" className="space-y-2">
           <h2 className="text-4xl font-bold leading-tight lg:text-5xl">
             Let's talk!
           </h2>
@@ -10,9 +15,9 @@ const Contact = () => {
             Vivamus in nisl metus? Phasellus.
           </div>
         </div>
-        <img src="assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" />
+        <Lottie animationData={contact} />
       </div>
-      <form noValidate="" className="space-y-6">
+      <form data-aos="fade-down-left" noValidate="" className="space-y-6">
         <div>
           <label htmlFor="name" className="text-sm">
             Full name
@@ -46,7 +51,7 @@ const Contact = () => {
         </div>
         <button
           type="submit"
-          className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-600 dark:text-gray-50"
+          className="w-full btn bg-[#fc5a34] text-white  hover:bg-[#000] p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-600 dark:text-gray-50"
         >
           Send Message
         </button>
